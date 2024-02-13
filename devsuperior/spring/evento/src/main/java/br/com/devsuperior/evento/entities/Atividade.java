c98a44408a15470ec457e19b6081ed4e8de12251
Ubirajara Junior <unjunior4315@gmail.com>
2024-02-13 14:15:08 -0300

    Iniciado projeto Evento utilizando Spring com Maven. Criado as entidades Categoria e Atividade

---
+++ devsuperior/spring/evento/src/main/java/br/com/devsuperior/evento/entities/Atividade.java
@@ -0,0 +1,21 @@
+package br.com.devsuperior.evento.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+}
