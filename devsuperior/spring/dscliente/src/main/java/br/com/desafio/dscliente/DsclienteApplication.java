43b317d39ccdd1f8723b9d7819918f079ede8272
Ubirajara Junior <unjunior4315@gmail.com>
2024-02-26 20:19:15 -0300

    Iniciado o projeto - CRUD completo web services REST para acessar recursos de clientes

---
+++ devsuperior/spring/dscliente/src/main/java/br/com/desafio/dscliente/DsclienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.dscliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsclienteApplication.class, args);
+	}
+
+}
---
+++ devsuperior/spring/dscliente/src/test/java/br/com/desafio/dscliente/DsclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.dscliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
