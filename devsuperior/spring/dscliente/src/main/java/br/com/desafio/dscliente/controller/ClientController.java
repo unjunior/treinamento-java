7989335df34fb1b87e9344a5bf3a75cd5085b5ca
Ubirajara Junior <unjunior4315@gmail.com>
2024-02-28 23:43:40 -0300

    adicionado busca paginada do recurso client

--- devsuperior/spring/dscliente/src/main/java/br/com/desafio/dscliente/controller/ClientController.java
+++ devsuperior/spring/dscliente/src/main/java/br/com/desafio/dscliente/controller/ClientController.java
@@ -3,10 +3,14 @@ package br.com.desafio.dscliente.controller;
 import br.com.desafio.dscliente.dto.ClientDTO;
 import br.com.desafio.dscliente.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -19,5 +23,10 @@ public class ClientController {
     public ClientDTO findById (@PathVariable Long id){
        ClientDTO dto = service.finbById(id);
        return dto;
+    }
+
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 }
--- devsuperior/spring/dscliente/src/main/java/br/com/desafio/dscliente/service/ClientService.java
+++ devsuperior/spring/dscliente/src/main/java/br/com/desafio/dscliente/service/ClientService.java
@@ -4,8 +4,12 @@ import br.com.desafio.dscliente.dto.ClientDTO;
 import br.com.desafio.dscliente.entities.Client;
 import br.com.desafio.dscliente.repository.ClientRepositoty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -17,5 +21,11 @@ public class ClientService {
     public ClientDTO finbById(Long id){
         Client client = repositoty.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repositoty.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
