4ec2a89e9643c11b9dbbd30824429283cd60d3b0
Ubirajara Junior <unjunior4315@gmail.com>
2024-02-28 21:54:13 -0300

    criado entidade cliente e seed de dados

---
+++ devsuperior/spring/dscliente/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('JOAO', '111111111-11', 1000.0, '1980-12-20', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('MARIA', '222222222-22', 2000.0, '1990-11-13', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('MARCOS', '333333333-33', 3000.0, '2000-01-09', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ANA', '444444444-44', 4000.0, '1992-08-14', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('JOAQUIM', '555555555-55', 4500.0, '1990-12-31', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('MARCELO', '666666666-66', 2800.0, '1988-05-25', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('FRANCISCA', '777777777-77', 3000.0, '1982-07-13', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ALEX', '888888888-88', 2950.0, '1991-04-13', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('MARTA', '999999999-99', 5000.0, '1976-08-19', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ENZO', '000000000-00', 1100.0, '2002-03-01', 0);
+
+
+
+
